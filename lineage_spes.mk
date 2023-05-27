4cf5f7998a22783e13758d87abf5c52be210c2bc
yesidev <96131314+yesidev@users.noreply.github.com>
2023-05-27 23:53:47 +0530

    Update lineage_spes.mk

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Additions
 #$(call inherit-product-if-exists, vendor/gms/common/common-vendor.mk)
@@ -24,51 +24,18 @@ $(call inherit-product-if-exists, vendor/spes-miuicamera/miuicamera.mk)
 # OyxgenOS
 $(call inherit-product-if-exists, vendor/OOS/OOS.mk)
 
-# Rising Stuff
-RISING_OFFICIAL := false
 
 # Aperture Camera
-TARGET_BUILD_APERTURE_CAMERA := false
+TARGET_BUILD_APERTURE_CAMERA := true
 
 # Blur Support
 TARGET_ENABLE_BLUR := true
 
-# Gapps
-WITH_GMS := false
-TARGET_CORE_GMS := false
 
-# extra flag under TARGET_CORE_GMS
-TARGET_CORE_GMS_EXTRAS := false
 
-#UDFPS animations
-TARGET_HAS_UDFPS := false
-
-# Spoof build description/fingerprint as pixel device
-TARGET_USE_PIXEL_FINGERPRINT := true
-
-#Assistant flags
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-
-#Face unlock supported
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-#Dolby flags
-TARGET_EXCLUDES_AUDIOFX := true
-
-#Google Telephony
-TARGET_USE_GOOGLE_TELEPHONY := false
-
-#Chipset
-RISING_CHIPSET := "Snapdragon-680"
-
-#Maintainer
-RISING_MAINTAINER := "Shripad-J"
-
-#Package type
-RISING_PACKAGE_TYPE := VANILLA
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := aosp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
